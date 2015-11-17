cf5002a0600591b45a6c2c57b234fd9eaecc8dc3
vijay.patolia@gmail.com <vijay.patolia@gmail.com>
2015-11-17 13:32:20 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testwebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testwebApp
+ */
+angular.module('testwebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
